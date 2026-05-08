fa7446fe29cec8802b73fb5e246aa07824d9772d
Vicky <vicky.vishal.sahu@gmail.com>
2026-05-08 10:42:25 +0200

    changes

--- app/property-creation/layout.tsx
+++ app/property-creation/layout.tsx
@@ -2,6 +2,6 @@ import type { Metadata } from 'next'
 
 export const metadata: Metadata = { title: 'Property creation' }
 
-const PropertiesLayout = ({ children }: { children: React.ReactNode }) => <>{children}</>
+const PropertyCreationLayout = ({ children }: { children: React.ReactNode }) => <>{children}</>
 
-export default PropertiesLayout
+export default PropertyCreationLayout
